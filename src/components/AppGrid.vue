27398f82a0db4a62585771a85dc3ede8dbd96ca8
Chicco80 <chicco80lisa@gmail.com>
2022-05-12 16:21:13 +0200

    modifiche layout

--- src/components/AppGrid.vue
+++ src/components/AppGrid.vue
@@ -1,34 +1,35 @@
 <template>
-    <section>
-        <h2 class="container">{{title}}</h2>
-            <div class="container debug ">
-                <div class="row">
-                    <ul class=" d-flex flex-wrap">
-                        <li v-for="item in items" :key="item.id">
-                            <div class="cont-poster">
-                                <img class="p-3 poster " :src="'https://image.tmdb.org/t/p/w342' + item.poster_path" alt="item.name">
-                            <div class="overlay d-flex p-3">
-                                <div class="text">{{item.overview}}</div>
-                            </div>
-                            <div class="card-body debug">
-                                <h2> {{item.original_title ? item.original_title : item.original_name}}</h2>
-                                <h4> {{item.title ? item.title : item.name}}</h4>
-                            <div v-if="item.original_language == 'en' && 'it'">
-                                <img :src="'../img/'+ item.original_language + '.png'" alt="item.name" class="icons">
-                            </div>
-                            <div v-else>
-                                <img :src="'../img/an.png'" alt="item.name" class="icons">
-                            </div>
-                            <span v-for="(n, index) in 5" :key="index">
-                                <i :class=" n <= showStar(item) ? 'fa-solid fa-star text-warning' : 'fa-regular fa-star'"></i>
-                            </span>
-                            </div>
-                            </div>
-                        </li>
-                    </ul>
+<section>
+    <h2 class="container">{{title}}</h2>
+    <div class="container">
+        <div class="row ">
+            <div  v-for="item in items" :key="item.id" class="col-4 my-3">
+                <div class="card d-flex">
+                    <img :src="'https://image.tmdb.org/t/p/w342' + item.poster_path" class="card-img-top" alt="item.name">
+
+                    <div class="overlay d-flex ">
+                        <div class="text">{{item.overview}}
+                            <div class="card-body">
+                                <h5 class="card-title">{{item.original_title ? item.original_title : item.original_name}}</h5>
+                                <h5>{{item.title ? item.title : item.name}}</h5>
+                                <div v-if="item.original_language == 'en' && 'it'">
+                                    <img :src="'../img/'+ item.original_language + '.png'" alt="item.name" class="icons">
+                                </div>
+                                <div v-else>
+                                    <img :src="'../img/an.png'" alt="item.name" class="icons">
+                                </div>
+                                <span v-for="(n, index) in 5" :key="index">
+                                    <i :class=" n <= showStar(item) ? 'fa-solid fa-star text-warning' : 'fa-regular fa-star'"></i>
+                                </span>
+                            </div>     
+                        </div>
+                    </div>
                 </div>
             </div>
-    </section> 
+        </div>
+    </div>
+
+</section>
 </template>
 
 <script>
@@ -56,11 +57,9 @@ export default {
     height: 100vh;
     width: 100%;
 }
-// img{
-//     width: 347px;
-// }
+
 .icons{
-    width:20px;
+    width:25px;
 }
 ul{
  display: flex;
@@ -71,32 +70,39 @@ ul{
 .debug{
     border: 1px solid red;
 }
-.card-body{
-
-    width: 342px;
-    
+.card{
+    background-color: black;
+    height: 100%;
 }
 
 .overlay{
+    align-items: center;
+    height: 100%;
+    width: 100%;
     position: absolute;
     top: 0;
     left: 0;
-    width: 342px;
+    right: 0;
+    bottom: 0;
     text-align: center;    
 }
 
 .text{
+    background: #000000;
+    opacity: 0.7;
+    height: 100%;
+    width: 100%;
     text-align: center;
     position: absolute;
-    opacity: 1;
-    padding-left: 20px;
+    padding:10px;
     display: none;
 
 }
-.cont-poster :hover .text{
-    display:block;
+ .card :hover .text{
+     display:block;
 }
-.cont-poster{
+.card-image-top{
+    height: 100%;
     width: 100%;
     position: relative;
 }
